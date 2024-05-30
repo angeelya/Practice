80e6c70175a3e96465eb0fc9861f51a91d002731
Angelina <n.angelina169@gmail.com>
2024-05-30 22:59:58 +0300

    Test custom QuickSort

---
+++ src/test/java/org/example/QuickSortTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuickSortTest {
+
+    @Test
+    void shouldSortViaComparable() {
+        ArrayCollection<User> users= new ArrayCollection<>();
+        users.add(new User("Danya",26,"st.Kira,34"));
+        users.add(new User("Anna",21,"st.Mira, 32"));
+        users.add(new User("Ilya",34,"st.Lenina, 23"));
+        users.add(new User("Alla",24,"st.Mira, 23"));
+        String firstName="Alla";
+        QuickSort.sort(users);
+        System.out.println(users);
+        assertEquals(users.get(0).getName(), firstName);
+    }
+
+    @Test
+    void shouldSortViaComparator() {
+        ArrayCollection<User> users= new ArrayCollection<>();
+        users.add(new User("Danya",26,"st.Kira,34"));
+        users.add(new User("Anna",21,"st.Mira, 32"));
+        users.add(new User("Ilya",34,"st.Lenina, 23"));
+        users.add(new User("Alla",24,"st.Mira, 23"));
+        Integer age=21;
+        QuickSort.sort(users, Comparator.comparing(User::getAge));
+        assertEquals(users.get(0).getAge(), age);
+    }
+}
